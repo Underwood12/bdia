c7ec08e4967052ad1378ae22f82a76a31167b8b7
easy-p <751383750@qq.com>
2019-07-22 00:26:41 +0800

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,63 @@
+struts: intitle:"Struts Problem Report" intext:"development mode is enabled." intext:贵州小学
+inurl:"struts" filetype:action intext:贵州小学
+?action=pro_show and ?action=disppro intext:贵州小学
+intext:build:SVNTag= JBoss intitle:Administration Console inurl:web-console intext:贵州小学
+intitle:”jboss management console” “application server” version inurl:”web-console” intext:贵州小学
+intitle:”JBoss Management Console – Server Information” “application server” inurl:”web-console” OR inurl:”jmx-console” intext:贵州小学
+inurl:jmx-console/HtmlAdaptor intext:贵州小学
+filetype:action intext:贵州小学
+filetype:action||ext:action intext:贵州小学
+inurl:Mail.action intext:贵州小学
+inurl:code.action intext:贵州小学
+inurl:reg.action intext:贵州小学
+inurl:Address.action intext:贵州小学
+inurl:!Index.action intext:贵州小学
+inurl:login.action intext:贵州小学
+inurl:Add.action intext:贵州小学
+inurl:pageslist.action intext:贵州小学
+inurl:action.Action intext:贵州小学
+inurl:Message.action intext:贵州小学
+inurl:Ldap.action intext:贵州小学
+inurl:getMul.action intext:贵州小学
+inurl:shouye.action intext:贵州小学
+inurl:logout.action intext:贵州小学
+inurl:Valid.action intext:贵州小学
+inurl:search.action intext:贵州小学
+inurl:Magazine.action intext:贵州小学
+inurl:news.action intext:贵州小学
+inurl:init.action intext:贵州小学
+inurl:Page.action intext:贵州小学
+inurl:create.action intext:贵州小学
+inurl:index2.action intext:贵州小学
+inurl:default.action intext:贵州小学
+inurl:welcome.action intext:贵州小学
+inurl:Name.action intext:贵州小学
+inurl:single.action intext:贵州小学
+inurl:updateForm.action intext:贵州小学
+inurl:SysStart.action intext:贵州小学
+inurl:adminlogin.action intext:贵州小学
+inurl:Offportal.action intext:贵州小学
+inurl:Buying.action intext:贵州小学
+inurl:Success.action intext:贵州小学
+inurl:exchange.action intext:贵州小学
+inurl:menu.action intext:贵州小学
+inurl:Load.action intext:贵州小学
+inurl:airport.action intext:贵州小学
+inurl:Email.action intext:贵州小学
+inurl:On.action intext:贵州小学
+inurl:show.action intext:贵州小学
+inurl:tain.action intext:贵州小学
+inurl:Login!input.action intext:贵州小学
+inurl:randomPicture.actionintext:贵州小学
+inurl:action  intext:贵州小学
+inurl:action?  intext:贵州小学
+inurl:action?id= intext:贵州小学
+inurl:struts2-showcase/index.action intext:贵州小学
+inurl:index.action intext:贵州小学
+intext:贵州小学 filetype:action
+intext:贵州小学 inurl:action
+intext:贵州小学 filetype:action
+inurl:.action?mid intext:贵州小学
+inurl:.action?name intext:贵州小学
+inurl:.action?pid intext:贵州小学
+inurl:.action?cid intext:贵州小学
